89d9d9b72315ff549eb113141852998a8c29e3d7
Aaryan Sachdeva <81976330+Aaryan221002@users.noreply.github.com>
2022-10-02 17:12:31 +0530

    Update largest-element-in-array.py

--- Python/largest-element-in-array.py
+++ Python/largest-element-in-array.py
@@ -1,8 +1,3 @@
 arr = [25, 11, 7, 75, 56];             
-max = arr[0];    
-for i in range(0, len(arr)):    
-    #Compare elements of array with max    
-   if(arr[i] > max):    
-       max = arr[i];    
-           
-print("Largest element present in given array: " + str(max));   
+arr.sort()
+print(arr[len(arr)-1]
